78bc257a6806c8f064cf5825b913d723513feacc
TharinduPerera0 <134123663+Tharinduakash@users.noreply.github.com>
2026-05-27 16:33:45 +0530

    added room hero image

--- components/sections/RoomsHero.tsx
+++ components/sections/RoomsHero.tsx
@@ -6,7 +6,7 @@ export default function RoomsHero() {
   return (
     <section className="relative h-[65vh] min-h-[450px] overflow-hidden flex items-end">
       <Image
-        src="/webp/IMG_3236.webp"
+        src="/webp/IMG_3873.webp"
         alt="Villa I Hotel Rooms"
         fill
         priority
